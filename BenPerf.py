41179fd18160eba1e20739beb1c6e8783b772c33
RaphaelHouver <raphael13410@gmail.com>
2021-07-31 12:19:14 +0200

    Modifying BenPerf.py

--- BenPerf.py
+++ BenPerf.py
@@ -12,15 +12,14 @@ soup = BeautifulSoup(data.text, "html.parser")
 #Récupération de Victoires/Défaites dans une liste "Streak"
 streak = []
 
-for div in soup.find_all("div", {"class":"victoryDefeatText victory"}):
-    print(div)
-    
-    """if int(div.text) > 0:
-        streak.append("Victoire")
-    else:
-        streak.append("Défaite")"""
+for td in soup.find_all("td", {"class":"championCellDark"}):
+    print(td)
+    """for div in td.find_all("div"):
+        if div.text == "Victoire" or div.text == "Défaite":
+            streak.append(div.text)"""
 
 print(streak)
+
 #Récupération du noms des joueurs de ses games pour voir s'il joue avec Guilleme
 names_per_game = []
 
